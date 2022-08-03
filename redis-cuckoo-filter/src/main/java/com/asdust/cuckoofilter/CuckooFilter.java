f72dbe62ef7e649120aa404cd34bce09332053f1
Jianxing.Huang2 <Jianxing.Huang2@geely.com>
2022-08-03 18:39:59 +0800

    feat: 增加方法注释

--- redis-cuckoo-filter/src/main/java/com/asdust/cuckoofilter/CuckooFilter.java
+++ redis-cuckoo-filter/src/main/java/com/asdust/cuckoofilter/CuckooFilter.java
@@ -7,17 +7,21 @@ import com.asdust.cuckoofilter.utils.Utils;
  * @date: 2022/7/31 17:35
  * @desc:
  */
-public class JCuckooFilter<T> {
+public class CuckooFilter<T> {
     private FilterTable table;
-    // 每个桶的大小为4，即可以存放4个key
-    static final int BUCKET_SIZE = 4;
-    // 负载因子,超过后，则以2倍扩容
+    /**
+     * 每个桶的大小为4，即可以存放4个key
+    */
+    private static final int BUCKET_SIZE = 4;
+    /**
+     * 负载因子,超过后，则以2倍扩容
+     */
     private static final double LOAD_FACTOR = 0.955;
     /**
      * TODO methods
      * @param max_num_keys 期望存放的最大key个数
      */
-    public JCuckooFilter(Long max_num_keys){
+    public CuckooFilter(Long max_num_keys){
         // 计算出实际的桶的个数
         long numBuckets = Utils.getBucketsNeeded(max_num_keys, LOAD_FACTOR, BUCKET_SIZE);
         table = new FilterTable(numBuckets);
@@ -25,19 +29,30 @@ public class JCuckooFilter<T> {
     }
 
     /**
-     * TODO methods
+     * 写入item
      * @param item
      * @return true or false
      */
     public boolean put(T item){
+        // 计算出索引和指纹
+
+        // 循环写入table，若写入成功则退出，写入失败，则弹出占位值，作为新值插入，如此循环知道插入成功，或者达到最大值
+
+        // 若不存在，则写入
+
+        // 若存在，则。。
 
         return true;
     }
     /**
-     * TODO methods
+     * 判断是否存在key
      * @return true or false
      */
     public boolean contain(){
+        // 计算出两个索引的位置
+
+        // 查询是否存在table中
+
         return true;
     }
     /** 
@@ -46,6 +61,10 @@ public class JCuckooFilter<T> {
      * @return true or false
      */
     public boolean delete(T item){
+        // 计算出两个索引的位置
+
+
+
         return true;
     }
 
--- redis-cuckoo-filter/src/main/java/com/asdust/cuckoofilter/FilterTable.java
+++ redis-cuckoo-filter/src/main/java/com/asdust/cuckoofilter/FilterTable.java
@@ -1,15 +1,21 @@
 package com.asdust.cuckoofilter;
 
 /**
- * author: chordCreater
+ * @author Jianxing.Huang2
  * date: 2022/8/1 23:13
- * desc：
- **/
+ */
 public class FilterTable {
-    // 最大容量
+    /**
+     * 最大容量
+    */
     private long numBuckets;
-    // 指纹字节数
+    /**
+     * 指纹字节数,最后通过计算公式得出最佳数量
+     */
+    private final int tagBits = 4;
     public FilterTable(long numBuckets){
         this.numBuckets = numBuckets;
     }
+
+
 }
